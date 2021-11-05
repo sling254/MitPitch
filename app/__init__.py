df32fad3c3435508015d6fdf4ddf3d2cf5def18a
sling254 <mwanikidenis7@gmail.com>
2021-11-05 15:47:09 +0300

    make migrations and add manager shell

--- app/__init__.py
+++ app/__init__.py
@@ -29,9 +29,13 @@ def create_app(config_name):
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+    
+    #Intializing flask extensions
+    db.init_app(app)#databse
+    login_manager.init_app(app)#login helper
+    mail.init_app(app)#mail
 
      # setting config
  
