b919a1d2b977475f9355535f74390a5b653c1753
sling254 <mwanikidenis7@gmail.com>
2021-11-05 14:50:27 +0300

    config development settings

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_login import  LoginManager
+from flask_mail import Mail
 
 
 
@@ -13,6 +14,9 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 #Upload files
+
+#send mail
+mail = Mail()
 
 
 def create_app(config_name):
