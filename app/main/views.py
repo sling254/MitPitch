4a52be76fa4c4fa3df3849c14201042cc0e38c23
sling254 <mwanikidenis7@gmail.com>
2021-11-05 15:58:12 +0300

    add function to handel the user profile page

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,22 @@
-from flask import render_template, url_for
+from flask import render_template, redirect, url_for,abort,request
 from . import main
+from flask_login import login_required,current_user
+from ..models import User,Pitch,Comment,Upvote,Downvote
+from .forms import UpdateProfile,PitchForm,CommentForm
+from .. import db,photos
 
 @main.route('/')
 def index():
 
     return 'Hello world'
+
+
+@main.route('/user/<name>')
+def profile(name):
+    user = User.query.filter_by(username = name).first()
+    user_id = current_user._get_current_object().id
+    posts = Pitch.query.filter_by(user_id = user_id).all()
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user,posts=posts)
