3489da95b924d4ab2fa3fe7b9a37f1831bcfc608
sling254 <mwanikidenis7@gmail.com>
2021-11-05 14:19:27 +0300

    add models

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,18 @@
 from flask import Flask
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_login import  LoginManager
+
+
+
+#database 
+db = SQLAlchemy()
+#login Hellper classes
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+#Upload files
 
 
 def create_app(config_name):
