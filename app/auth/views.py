1560dc7f867446dd49b3adb2a89f1d54d07a44ad
sling254 <mwanikidenis7@gmail.com>
2021-11-05 14:29:11 +0300

    move forms to auth and add a view to register user

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,11 @@
-from flask import render_template,redirect,url_for, flash,request
+from flask import render_template,url_for,flash,redirect,request
 from . import auth
+from flask_login import login_user,login_required,logout_user
+from .forms import RegForm,LoginForm
+from ..models import User
+from .. import db
+from ..email import mail_message
+
 
 
 @auth.route('/login')
@@ -8,7 +14,13 @@ def login():
     return render_template('auth/login.html')
 
 
-@auth.route('/register')
-def register():
 
-    return render_template('auth/register.html')
+@auth.route('/register', methods = ["GET","POST"])
+def register():
+  form = RegForm()
+  if form.validate_on_submit():
+    user = User(email = form.email.data, username = form.username.data, password = form.password.data)
+    user.save_u()
+    mail_message("Welcome to the Pitch","email/welcome_user",user.email,user=user)
+    return redirect(url_for('auth.login'))
+  return render_template('auth/register.html', r_form = form)
