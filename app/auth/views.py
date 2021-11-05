abf99fed43f3efa586b24d956ad8528b2fdc1e3a
sling254 <mwanikidenis7@gmail.com>
2021-11-05 14:31:26 +0300

    add register.html

--- app/auth/views.py
+++ app/auth/views.py
@@ -23,4 +23,4 @@ def register():
     user.save_u()
     mail_message("Welcome to the Pitch","email/welcome_user",user.email,user=user)
     return redirect(url_for('auth.login'))
-  return render_template('auth/register.html', r_form = form)
+  return render_template('auth/register.html', form = form)
