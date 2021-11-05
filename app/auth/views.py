b82e6bd702545ce43a302f179acf99e274960712
sling254 <mwanikidenis7@gmail.com>
2021-11-05 15:07:18 +0300

    add function to handel login

--- app/auth/views.py
+++ app/auth/views.py
@@ -8,7 +8,16 @@ from ..email import mail_message
 
 
 
-
+@auth.route('/login', methods = ['GET','POST'])
+def login():
+  form = LoginForm()
+  if form.validate_on_submit():
+    user = User.query.filter_by(email = form.email.data).first()
+    if user != None and user.verify_password(form.password.data):
+      login_user(user,form.remember.data)
+      return redirect(request.args.get('next') or url_for('main.index'))
+    flash('Invalid username or Password')
+  return render_template('auth/login.html', form = form)
 
 
 
